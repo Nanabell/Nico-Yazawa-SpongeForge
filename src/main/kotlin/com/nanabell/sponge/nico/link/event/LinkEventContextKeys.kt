c056774f3f955dd421d17bc89fabd0a9b8c4f38a
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-20 22:09:29 +0100

    Remove unused LINK_RESULT LinkEventContextKeys

--- src/main/kotlin/com/nanabell/sponge/nico/link/event/LinkEventContextKeys.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/link/event/LinkEventContextKeys.kt
@@ -1,6 +1,5 @@
 package com.nanabell.sponge.nico.link.event
 
-import com.nanabell.sponge.nico.link.LinkResult
 import net.dv8tion.jda.api.entities.MessageChannel
 import net.dv8tion.jda.api.entities.User
 import org.spongepowered.api.event.cause.EventContextKey
@@ -9,7 +8,6 @@ import org.spongepowered.api.util.generator.dummy.DummyObjectProvider
 object LinkEventContextKeys {
     val USER = createFor<User>("DISCORD_USER")
     val MESSAGE_CHANNEL = createFor<MessageChannel>("DISCORD_MESSAGE_CHANNEL")
-    val LINK_RESULT = createFor<LinkResult>("Nico-Yazawa:LINK_RESULT")
 
     @Suppress("UNCHECKED_CAST")
     private fun <T> createFor(id: String): EventContextKey<T> {
