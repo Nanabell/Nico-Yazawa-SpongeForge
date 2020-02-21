96e3af9bac23f56e8eb319fbbf1c5bd102c79d11
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-21 19:53:33 +0100

    Migrate PaymentConfig to data class

--- src/main/kotlin/com/nanabell/sponge/nico/config/PaymentConfig.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/config/PaymentConfig.kt
@@ -4,25 +4,24 @@ import ninja.leaping.configurate.objectmapping.Setting
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable
 
 @ConfigSerializable
-class PaymentConfig(paymentAmount: Int = 50, dailyPaymentLimit: Int = 1500, paymentChance: Int = 100, requiredPermission: String = "") {
-    @Setting(value = "payment-amount", comment = "Amount Money that is rewarded [0 - 2,147,483,647]")
-    var paymentAmount = paymentAmount
-        get() = field.coerceAtLeast(0)
-        private set
+data class PaymentConfig(
 
-    @Setting(value = "payment-limit", comment = "Maximum amount of currency that can be earned per day [0 = no limit, 2,147,483,647 = max]")
-    var dailyPaymentLimit = dailyPaymentLimit
-        get() = field.coerceAtLeast(0)
-        private set
+        @Setting(value = "payment-amount", comment = "Amount Money that is rewarded [0 - 2,147,483,647]")
+        val _paymentAmount: Long = 50,
 
-    @Setting(value = "payment-chance", comment = "Percentage chance for payment [0 = never, 100 = always]")
-    var paymentChance = paymentChance
-        get() = field.coerceIn(0, 100)
-        private set
+        @Setting(value = "payment-limit", comment = "Maximum amount of currency that can be earned per day [0 = no limit, 2,147,483,647 = max]")
+        val _dailyPaymentLimit: Long = 1500,
 
-    @Setting(value = "required-permission", comment = "Required Permission to be eligible for this Payment [Empty for no requirement]")
-    var requiredPermission: String = requiredPermission
-        private set
+        @Setting(value = "payment-chance", comment = "Percentage chance for payment [0 = never, 100 = always]")
+        val _paymentChance: Int = 100,
+
+        @Setting(value = "required-permission", comment = "Required Permission to be eligible for this Payment [Empty for no requirement]")
+        val requiredPermission: String = ""
+
+      ) {
+        val paymentAmount get() = _paymentAmount.coerceAtLeast(0)
+        val dailyPaymentLimit get() = _dailyPaymentLimit.coerceAtLeast(0)
+        val paymentChance get() = _paymentChance.coerceIn(0, 100)
 
     companion object {
 
