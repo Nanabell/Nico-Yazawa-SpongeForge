54813fbe2e8615440130a5480534006d699dc44c
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-21 19:39:47 +0100

    Change Config "_" to "-" Part #2

--- src/main/kotlin/com/nanabell/sponge/nico/config/PaymentConfig.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/config/PaymentConfig.kt
@@ -10,7 +10,7 @@ class PaymentConfig(paymentAmount: Int = 50, dailyPaymentLimit: Int = 1500, paym
         get() = field.coerceAtLeast(0)
         private set
 
-    @Setting(value = "payment_limit", comment = "Maximum amount of currency that can be earned per day [0 = no limit, 2,147,483,647 = max]")
+    @Setting(value = "payment-limit", comment = "Maximum amount of currency that can be earned per day [0 = no limit, 2,147,483,647 = max]")
     var dailyPaymentLimit = dailyPaymentLimit
         get() = field.coerceAtLeast(0)
         private set
