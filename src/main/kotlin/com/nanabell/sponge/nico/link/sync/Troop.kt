57627f6e38afe2aa8c235f960d59051410c03c28
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-23 18:49:28 +0100

    Add missing ] to Troop#toString()

--- src/main/kotlin/com/nanabell/sponge/nico/link/sync/Troop.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/link/sync/Troop.kt
@@ -4,8 +4,8 @@ import com.nanabell.sponge.nico.link.discord.DiscordService
 import org.spongepowered.api.Sponge
 
 data class Troop(
-        val permission: String,
         val role: String,
+        val permission: String,
         val source: TroopSource
 ) {
 
@@ -15,7 +15,7 @@ data class Troop(
     fun getRoleName(): String = discordService.getRole(role)?.name ?: "[ERR] Role ($role) not found!"
 
     override fun toString(): String {
-        return if (source == TroopSource.MINECRAFT) "Troop: [group $permission ==> role $role, source: $source"
-        else "Troop: [role $role ==> group $permission, source: $source"
+        return (if (source == TroopSource.MINECRAFT) "Troop: [group $permission ==> role $role, source: $source"
+        else "Troop: [role $role ==> group $permission, source: $source") + "]"
     }
 }
