ac58fce64ed568ec4d282976e04181bc1e738df5
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-25 18:57:33 +0100

    Implement AbstractService

--- src/main/kotlin/com/nanabell/sponge/nico/internal/service/AbstractService.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/internal/service/AbstractService.kt
@@ -1,4 +1,26 @@
 package com.nanabell.sponge.nico.internal.service
 
-abstract class AbstractService {
+import com.nanabell.sponge.nico.NicoYazawa
+import com.nanabell.sponge.nico.internal.module.ConfigurableModule
+import com.nanabell.sponge.nico.internal.module.StandardModule
+import org.slf4j.Logger
+
+abstract class AbstractService<M : ConfigurableModule<*>> {
+
+    protected val plugin: NicoYazawa = NicoYazawa.getPlugin()
+    protected val logger: Logger
+
+    protected lateinit var module: M
+
+    init {
+        this.logger = plugin.getLogger("Service", javaClass.simpleName)
+    }
+
+    abstract fun onEnable()
+
+    @Suppress("UNCHECKED_CAST")
+    fun setModule(module: StandardModule) {
+        this.module = module as M
+    }
+
 }
