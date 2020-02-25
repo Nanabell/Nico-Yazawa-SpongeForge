b27e960346220718c13dc438d0394d7bc8c6f2ae
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-25 16:40:58 +0100

    Make RegisterService#value an KClass<in AbstractService>

--- src/main/kotlin/com/nanabell/sponge/nico/internal/annotation/RegisterService.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/internal/annotation/RegisterService.kt
@@ -1,10 +1,11 @@
 package com.nanabell.sponge.nico.internal.annotation
 
+import com.nanabell.sponge.nico.internal.service.AbstractService
 import kotlin.reflect.KClass
 
 @Target(AnnotationTarget.ANNOTATION_CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 annotation class RegisterService(
-        val value: KClass<*>,
+        val value: KClass<in AbstractService>,
         val override: Boolean = false
 )
