733d2b85e61123b9067e42d8c6f07b5353538606
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-25 18:58:15 +0100

    Update ServiceBuilder for new Annotations

--- src/main/kotlin/com/nanabell/sponge/nico/internal/service/ServiceBuilder.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/internal/service/ServiceBuilder.kt
@@ -3,7 +3,8 @@ package com.nanabell.sponge.nico.internal.service
 import com.nanabell.sponge.nico.NicoYazawa
 import com.nanabell.sponge.nico.internal.annotation.ApiService
 import com.nanabell.sponge.nico.internal.annotation.RegisterService
-import org.slf4j.Logger
+import com.nanabell.sponge.nico.internal.module.ConfigurableModule
+import com.nanabell.sponge.nico.internal.module.StandardModule
 import org.spongepowered.api.Sponge
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
@@ -12,30 +13,33 @@ import kotlin.reflect.full.isSubclassOf
 
 class ServiceBuilder(
         private val plugin: NicoYazawa,
-        private val logger: Logger
+        private val module: StandardModule
 ) {
 
+    private val logger = NicoYazawa.getPlugin().getLogger("ServiceBuilder")
+
     @Suppress("UNCHECKED_CAST")
-    fun <T : AbstractService> register(clazz: KClass<T>) {
+    fun <T : AbstractService<out ConfigurableModule<*>>> register(clazz: KClass<T>) {
         val rs = clazz.findAnnotation<RegisterService>()
         if (rs == null) {
             logger.warn("Service Class {} is not Annotated with required Annotation @RegisterService", clazz)
             return
         }
+        val key = if (rs.value == AbstractService::class) clazz else rs.value
 
-        val service = clazz.createInstance()
+        val service: T = clazz.createInstance()
         if (!service::class.isSubclassOf(rs.value)) {
             logger.error("Service {} is not a subclass of {}", service::class, rs.value)
             return
         }
 
         if (clazz.findAnnotation<ApiService>() != null) {
-            if (Sponge.getServiceManager().isRegistered(rs.value.java) && !rs.override) {
+            if (Sponge.getServiceManager().isRegistered(key.java as Class<Any>) && !rs.override) {
                 logger.warn("There already is a Service registered for {}", rs.value)
                 return
             }
 
-            Sponge.getServiceManager().setProvider(plugin, rs.value.java, service)
+            Sponge.getServiceManager().setProvider(plugin, rs.value.java as Class<Any>, service)
         } else {
             if (plugin.getServiceRegistry().isRegistered(rs.value) && !rs.override) {
                 logger.warn("There already is an Internal Service registered for {}", rs.value)
@@ -44,5 +48,8 @@ class ServiceBuilder(
 
             plugin.getServiceRegistry().register(rs.value, service)
         }
+
+        service.setModule(module)
+        service.onEnable()
     }
 }
