a65167f7cb58f935c1f49bcdfb487bc50f384016
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-26 19:49:12 +0100

    Old Config cleanup

--- src/main/kotlin/com/nanabell/sponge/nico/config/MainConfig.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/config/MainConfig.kt
@@ -6,14 +6,8 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable
 @ConfigSerializable
 data class MainConfig(
 
-        @Setting(comment = "Database URL used by Nico to store immediate data")
-        val databaseUrl: String = "mongodb://user:password@host/database",
-
         @Setting(value = "activity", comment = "Nico Points Activity Settings. Gain NicoPoints by being active in Minecraft")
         val activityConfig: ActivityConfig = ActivityConfig(),
-
-        @Setting(value = "discord", comment = "Discord Linking Settings. \"Authenticate\" Minecraft users with Discord")
-        val discordLinkConfig: DiscordLinkConfig = DiscordLinkConfig(),
 
         @Setting(value = "economy")
         val economyConfig: EconomyConfig = EconomyConfig()
