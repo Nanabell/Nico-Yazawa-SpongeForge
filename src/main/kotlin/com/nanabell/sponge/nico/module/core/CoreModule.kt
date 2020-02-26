d39dea866642f690a542a8f22912d82a19794be4
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-26 10:16:57 +0100

    Remove doubled loggings from CoreModule

--- src/main/kotlin/com/nanabell/sponge/nico/module/core/CoreModule.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/module/core/CoreModule.kt
@@ -7,17 +7,4 @@ import uk.co.drnaylor.quickstart.annotations.ModuleData
 @ModuleData(id = "core", name = "Core Module")
 class CoreModule : ConfigurableModule<CoreConfigAdapter>(CoreConfigAdapter()) {
 
-    override fun performPreEnable() {
-        logger.info("Constructing Module...")
-    }
-
-    override fun performEnable() {
-        logger.info("Hello World!")
-    }
-
-    override fun performPostEnable() {
-        logger.info("Construction complete!")
-    }
-
-
 }
