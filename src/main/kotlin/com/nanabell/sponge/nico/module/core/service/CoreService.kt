632d598a916135ae43c5725fcc6698357db953d7
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-26 10:17:36 +0100

    Update CoreService logging to include if @ApiService worked

--- src/main/kotlin/com/nanabell/sponge/nico/module/core/service/CoreService.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/module/core/service/CoreService.kt
@@ -5,12 +5,13 @@ import com.nanabell.sponge.nico.internal.annotation.RegisterService
 import com.nanabell.sponge.nico.internal.service.AbstractService
 import com.nanabell.sponge.nico.module.core.CoreModule
 import com.nanabell.sponge.nico.module.core.`interface`.CoreServiceInterface
+import org.spongepowered.api.Sponge
 
 @ApiService
 @RegisterService(CoreServiceInterface::class)
 class CoreService : AbstractService<CoreModule>(), CoreServiceInterface {
 
     override fun onEnable() {
-        logger.info("Hello World! StartupError: ${module.getTypedConfigAdapter().nodeOrDefault.startupError}")
+        logger.info("Hello World! Api Register Status: ${Sponge.getServiceManager().isRegistered(CoreServiceInterface::class.java)}")
     }
 }
