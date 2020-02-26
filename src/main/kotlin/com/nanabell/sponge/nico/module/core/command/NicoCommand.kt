8a2b7f9d8cd0bbd2a03d1ca42ad58732baab87ef
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-26 11:53:32 +0100

    Add base Nico Command and move DummyCommand to Sub of Nico

--- src/main/kotlin/com/nanabell/sponge/nico/module/core/command/NicoCommand.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/module/core/command/NicoCommand.kt
@@ -2,7 +2,7 @@ package com.nanabell.sponge.nico.module.core.command
 
 import com.nanabell.sponge.nico.internal.annotation.command.Permissions
 import com.nanabell.sponge.nico.internal.annotation.command.RegisterCommand
-import com.nanabell.sponge.nico.internal.command.AbstractCommand
+import com.nanabell.sponge.nico.internal.command.StandardCommand
 import com.nanabell.sponge.nico.module.core.CoreModule
 import org.spongepowered.api.command.CommandResult
 import org.spongepowered.api.command.CommandSource
@@ -10,15 +10,13 @@ import org.spongepowered.api.command.args.CommandContext
 import org.spongepowered.api.event.cause.Cause
 
 @Permissions
-@RegisterCommand(["dummy"], NicoCommand::class)
-class DummyCommand : AbstractCommand<CommandSource, CoreModule>() {
+@RegisterCommand(["nico"], hasExecutor = false)
+class NicoCommand : StandardCommand<CoreModule>() {
 
     override fun executeCommand(source: CommandSource, args: CommandContext, cause: Cause): CommandResult {
-        logger.info("Hello World!")
-        return CommandResult.success()
+        throw IllegalStateException("This Command cannot be executed!")
     }
 
-    override fun getDescription(): String {
-        return "Admin Dummy Command. be careful to use this. This might literally do anything!!"
-    }
+    override fun getDescription(): String = "Base Nico Command"
+
 }
