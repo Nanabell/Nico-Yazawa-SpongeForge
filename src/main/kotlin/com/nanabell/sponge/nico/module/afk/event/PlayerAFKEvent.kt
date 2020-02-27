c507217eedbbef3ce479a9df2eb1683784c7a3da
mhaenen <Marlon.Haenen@it-novum.com>
2020-02-27 12:49:47 +0100

    Add getInactivityDuration to PlayerAFKEvent

--- src/main/kotlin/com/nanabell/sponge/nico/module/afk/event/PlayerAFKEvent.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/module/afk/event/PlayerAFKEvent.kt
@@ -1,14 +1,18 @@
 package com.nanabell.sponge.nico.module.afk.event
 
 import com.nanabell.sponge.nico.internal.event.StandardCancellableEvent
-import com.nanabell.sponge.nico.module.afk.data.AfkPlayer
 import org.spongepowered.api.entity.living.player.Player
 import org.spongepowered.api.event.cause.Cause
 import org.spongepowered.api.event.entity.living.humanoid.player.TargetPlayerEvent
+import java.time.Duration
 import java.time.Instant
 
-class PlayerAFKEvent(inactiveSince: Instant, private val player: Player, cause: Cause) : StandardCancellableEvent(cause), TargetPlayerEvent {
+class PlayerAFKEvent(private val inactiveSince: Instant, private val player: Player, cause: Cause) : StandardCancellableEvent(cause), TargetPlayerEvent {
 
     override fun getTargetEntity(): Player = player
+
+    fun getInactivityDuration(): Duration {
+        return Duration.between(Instant.now(), inactiveSince)
+    }
 
 }
