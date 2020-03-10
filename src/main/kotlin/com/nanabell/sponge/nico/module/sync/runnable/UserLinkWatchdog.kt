108ca792f58edddcde1d0d23fe477fc87cbbb3eb
mhaenen <Marlon.Haenen@it-novum.com>
2020-03-10 22:16:17 +0100

    Migrate UserLinkWatchdog to quartz scheduling

--- src/main/kotlin/com/nanabell/sponge/nico/module/sync/runnable/UserLinkWatchdog.kt
+++ src/main/kotlin/com/nanabell/sponge/nico/module/sync/runnable/UserLinkWatchdog.kt
@@ -1,39 +1,47 @@
 package com.nanabell.sponge.nico.module.sync.runnable
 
 import com.nanabell.sponge.nico.NicoYazawa
-import com.nanabell.sponge.nico.internal.annotation.RegisterRunnable
+import com.nanabell.sponge.nico.internal.annotation.RegisterSchedule
 import com.nanabell.sponge.nico.internal.extension.formatDefault
 import com.nanabell.sponge.nico.internal.extension.toText
-import com.nanabell.sponge.nico.internal.runnable.AbstractRunnable
+import com.nanabell.sponge.nico.internal.schedule.AbstractSchedule
 import com.nanabell.sponge.nico.module.activity.service.PlaytimeService
 import com.nanabell.sponge.nico.module.link.service.LinkService
 import com.nanabell.sponge.nico.module.sync.SyncModule
-import com.nanabell.sponge.nico.module.sync.config.KickConfig
+import org.quartz.*
 import org.spongepowered.api.Sponge
-import java.util.concurrent.TimeUnit
 
-@RegisterRunnable("NicoYazawa-A-LinkWatchdog", isAsync = true)
-class UserLinkWatchdog : AbstractRunnable<SyncModule>() {
+@RegisterSchedule
+class UserLinkWatchdog : AbstractSchedule<SyncModule>() {
 
-    private val linkService: LinkService = NicoYazawa.getServiceRegistry().provideUnchecked()
-    private val playtimeService: PlaytimeService = NicoYazawa.getServiceRegistry().provideUnchecked()
-
-    private lateinit var config: KickConfig
-
-    override fun onReady() {
-        this.config = module.getConfigOrDefault().kickConfig
+    override fun registerJobs(): List<JobDetail> {
+        return listOf(
+                JobBuilder.newJob(UserLinkWatchdog::class.java)
+                        .withIdentity(UserLinkWatchdog::class.simpleName, SyncModule::class.simpleName)
+                        .storeDurably()
+                        .build()
+        )
     }
 
-    override fun overrideDelay(): Pair<Long, TimeUnit>? {
-        return (config.kickInterval * 2) to TimeUnit.SECONDS
+    override fun registerTriggers(): List<Trigger> {
+        return listOf(
+                TriggerBuilder.newTrigger()
+                        .withIdentity("${UserLinkWatchdog::class.simpleName}Trigger", SyncModule::class.simpleName)
+                        .forJob(UserLinkWatchdog::class.simpleName, SyncModule::class.simpleName)
+                        .withSchedule(SimpleScheduleBuilder.simpleSchedule()
+                                .withIntervalInMinutes(5)
+                                .repeatForever())
+                        .startAt(DateBuilder.futureDate(5, DateBuilder.IntervalUnit.MINUTE))
+                        .build()
+        )
     }
 
-    override fun overrideInterval(): Pair<Long, TimeUnit>? {
-        return config.kickInterval to TimeUnit.SECONDS
-    }
+    override fun execute(context: JobExecutionContext) {
+        val config = (context.mergedJobDataMap["module"] as SyncModule).getConfigOrDefault().kickConfig
+        val playtimeService: PlaytimeService = NicoYazawa.getServiceRegistry().provideUnchecked()
+        val linkService: LinkService = NicoYazawa.getServiceRegistry().provideUnchecked()
 
-    override fun run() {
-        if (!config.enabled) return
+        if (!config.enabled) return // TODO: Pause Job if disabled
 
         for (player in Sponge.getServer().onlinePlayers) {
             if (linkService.isLinked(player)) { // TODO: Possibly in the future retrieve all links at once
@@ -47,9 +55,5 @@ class UserLinkWatchdog : AbstractRunnable<SyncModule>() {
             }
         }
         logger.debug("Finished UserLinkWatchdog")
-    }
-
-    override fun onReload() {
-        this.config = module.getConfigOrDefault().kickConfig
     }
 }
